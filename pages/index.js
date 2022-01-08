d6907a08c0dd01f3a7027a87a02d41033a8498ce
sanjaybaskaran01 <72266283+sanjaybaskaran01@users.noreply.github.com>
2022-01-08 16:35:24 +0530

    Initialized with Next.js and Tailwind

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className="text-center">
+      This is a <a href="https://nextjs.org/" target="_blank">Next.js</a> project bootstrapped with <a href="https://github.com/vercel/next.js/tree/canary/packages/create-next-app" target="_blank">create-next-app</a> and <a href="https://tailwindcss.com/docs/guides/nextjs" target="_blank">Tailwind</a>
+    </div>
+  )
+}
